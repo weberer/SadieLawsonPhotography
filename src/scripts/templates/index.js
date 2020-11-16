8181d3af7208cf7369c674a295021c2928890d84
Eric Weber <epweber@nglic.com>
2020-11-16 10:12:52 -0600

    Fixed spelling mistakes on the main 'about-me' page

--- src/scripts/templates/index.js
+++ src/scripts/templates/index.js
@@ -9,14 +9,14 @@ export default () => `${getHeader(getPageName(PAGE_IDS.INDEX))}
             <h5 class="center primary-text-dark">Hi I'm Sadie!</h5>
             <p>
                 <img id="sadie-photo" src="${sadiePhoto.default}" alt="Meet Me">
-                Photography came to me at a time when I needed it most- it was like walking into what I was created to do. 
+                Photography came to me at a time when I needed it most - it was like walking into what I was created to do. 
                 It fits me; my personality and my strengths.
                 I live passionately, all in. I want today to be different than yesterday and full of excitement for tomorrow. 
                 Capturing moments as they happen and creating relationships is what photography means to me. 
                 When you choose me as your photographer, you are choosing a friend to capture the most intimate moments of your life.
                 The people closest to me would describe me as energetic, personable, and the right amount of sass. 
-                When I am not behind I am a mom to a two year old little girl, and two cats. The wife to an amazing husband 
-                (who is my number 1 fan and biggest supporter, and full time social worker.
+                When I am not behind the camera, I am a mom to a two year old little girl and two cats, the wife to an amazing husband 
+                (who is my number 1 fan and biggest supporter), and full time social worker.
                 <br><br>
                 <img id="family-photo" src="${family.default}" alt="Meet My Family">
                 Thank you for taking the time to “meet me”. I can’t wait to be a part of your life journey!
