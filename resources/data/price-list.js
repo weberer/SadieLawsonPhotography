e68497e47c9d2d90490ba8d21b170fb75b3d506f
Eric Weber <epweber@nglic.com>
2020-11-30 01:21:39 -0600

    Updated formatting of price data. Updated styling of investment cards

--- resources/data/price-list.js
+++ resources/data/price-list.js
@@ -1,7 +1,9 @@
 //const _PHOTO_DIR = '../resources/photos/';
+const _formatPrice = price => `&mdash;${price}&mdash;`
+const _mileageStatement = '* Mileage is calculated as one-way travel';
 
 export default [
-    {
+/*    {
     id: 0,
     name: 'Hot CoCo  Mini-Session',
     subtitle: 'November 7th 8th 3:00pm - 5:30pm',
@@ -13,29 +15,27 @@ export default [
 //    image: _PHOTO_DIR  + 'winter-mini/winter-mini-1 - Copy.jpg',
     ticketLink: 'https://www.eventbrite.com/e/2020-holiday-mini-sessions-tickets-122913907807',
 //    largeWidth: '8'
-  },
+  },*/
   {
     id: 1,
     name: 'Weddings',
     subtitle: '2, 4, 6 and 8 Hour Options Available',
-    price: '&mdash;Starting at $200&mdash;',
-    description: `Wedding Packages Include:
-    <ul>
-      <li>50 Curated and Edited Photos</li>
-      <li>2 Hours of Photographer Time</li>
-      <li>Complementary Engagement Session <strong>OR</strong> 10 Additional Photos</li>
-      <li>20 Miles of Photographer Travel*</li>
-      <li>Digital Gallery</li>
-      <li>Print Rights</li>
-    </ul>
-    <span class="primary-accent-text">Optional Add-Ons:</span>
-    <ul>
-      <li>Additional Photos - $10 Each</li>
-      <li>Unlimited Photos - $200</li>
-      <li>Additional Time - $200 per 2 Hours</li>
-      <li>Extended Travel Distance - $10 per 10 Miles</li>
-    </ul>
-    <span class="text-small primary-accent-text">* Mileage is calculated as one-way travel</span>`,
+    price: _formatPrice('Starting at $200'),
+    included: [
+        "50 Curated and Edited Photos",
+        "2 Hours of Photographer Time",
+        "Complementary Engagement Session <strong>OR</strong> 10 Additional Photos",
+        "20 Miles of Photographer Travel*",
+        "Digital Gallery",
+        "Print Rights"
+    ],
+    addOns: [
+        "Additional Photos - $10 Each",
+        "Unlimited Photos - $200",
+        "Additional Time - $200 per 2 Hours",
+        "Extended Travel Distance - $10 per 10 Miles*"
+    ],
+    finePrint: _mileageStatement
   },
   /*{
     id: 2,
@@ -58,171 +58,155 @@ export default [
     id: 3,
     name: 'Engagement Session',
     subtitle: '1 Hour Shoot',
-    price: '&mdash;Packages Starting at $100&mdash;',
-    description: `Engagement Sessions Include:
-    <ul>
-      <li>15 Curated and Edited Photos</li>
-      <li>Single Location</li>
-      <li>20 Miles of Photographer Travel*</li>
-      <li>Digital Gallery</li>
-      <li>Print Rights</li>
-    </ul>
-    <span class="primary-accent-text">Optional Add-Ons:</span>
-    <ul>
-      <li>Additional Photos - $10 Each</li>
-      <li>Second Location - $50</li>
-      <li>Extended Travel Distance - $10 per 10 Miles</li>
-    </ul>
-    <span class="text-small primary-accent-text">* Mileage is calculated as one-way travel</span>`
+    price: _formatPrice('Starting at $100'),
+    included: [
+        "15 Curated and Edited Photos",
+        "Single Location",
+        "20 Miles of Photographer Travel*",
+        "Digital Gallery",
+        "Print Rights"
+    ],
+    addOns: [
+        "Additional Photos - $10 Each",
+        "Second Location - $50",
+        "Extended Travel Distance - $10 per 10 Miles*"
+    ],
+    finePrint: _mileageStatement
   },
   {
     id: 4,
     name: 'Family Portrait',
     subtitle: '30 Minute Shoot',
-    price: '&mdash;Starting at $100&mdash;',
-    description: `Family Portrait Sessions Include: 
-    <ul>
-        <li>10+ Curated and Edited Photos</li>
-        <li>Up to 4 Family Members (Not Including Pets)</li>
-        <li>1 Outfit per Family Member</li>
-        <li>1 Location</li>
-        <li>20 Miles of Photographer Travel*</li>
-        <li>Digital Gallery</li>
-        <li>Print Rights</li>
-    </ul>
-    <span class="primary-accent-text">Optional Add-Ons:</span>
-    <ul>
-      <li>Additional Family Members - $10 Each</li>
-      <li>Additional Outfits - $20 per Outfit</li>
-      <li>Additional Location - $20 per Location</li>
-      <li>Extended Travel Distance - $10 per 10 Miles</li>
-    </ul>
-    <span class="text-small primary-accent-text">* Mileage is calculated as one-way travel</span>`,
+    price: _formatPrice('Starting at $100'),
+    included: [
+        "10+ Curated and Edited Photos",
+        "Up to 4 Family Members (Not Including Pets)",
+        "1 Outfit per Family Member",
+        "1 Location",
+        "20 Miles of Photographer Travel*",
+        "Digital Gallery",
+        "Print Rights"
+    ],
+    addOns: [
+        "Additional Family Member - $10",
+        "Additional Outfit - $10",
+        "Additional Location - $20",
+        "Extended Travel Distance - $10 per 10 Miles*"
+    ],
+    finePrint: _mileageStatement
   },
   {
     id: 5,
     name: 'Senior Portrait',
-    subtitle: 'It\'s finally here, senior year!! Let me help you remember this life milestone',
-    price: '&mdash;Starting at $150&mdash;',
-    description: `Senior Portrait Sessions Include:
-    <ul>
-        <li>7 Photos</li>      
-        <li>2 Outfits</li>
-        <li>2 Locations (Indoor, Outdoor, or Both)</li>
-        <li>20 Miles of Photographer Travel*</li>
-        <li>Consulting session before the shoot to help you look your best</li>  
-        <li>Digital Gallery</li>
-        <li>Print Rights</li>
-    </ul>
-    <span class="primary-accent-text">Optional Add-Ons:</span>
-    <ul>
-      <li>Additional Outfits - $20 per Outfit</li>
-      <li>Additional Location - $20 per Location</li>
-      <li>Extended Travel Distance - $10 per 10 Miles</li>
-    </ul>
-    <span class="text-small primary-accent-text">* Mileage is calculated as one-way travel, and includes photographer travel to the first location, and between locations</span>`,
+    subtitle: "It's finally here, senior year!! Let me help you remember this life milestone",
+    price: _formatPrice('Starting at $150'),
+    included: [
+        "7 Photos",
+        "2 Outfits",
+        "2 Locations (Indoor, Outdoor, or Both)",
+        "20 Miles of Photographer Travel*",
+        "Consulting session before the shoot to help you look your best",
+        "Digital Gallery",
+        "Print Rights"
+    ],
+    addOns: [
+        "Additional Outfit - $20",
+        "Additional Location - $20",
+        "Extended Travel Distance - $10 per 10 Miles*"
+    ],
+    finePrint: _mileageStatement + ". This includes travel to both the first location, and between multiple locations."
   },
   {
     id: 6,
     name: "Professional Headshots",
-    subtitle: "",
-    price: "&mdash;Starting at $50&mdash;",
+    price: _formatPrice('Starting at $50'),
     description: `Professional Headshot Sessions typically last between 15 and 30 minutes depending on your needs.<br>
-    Group pricing options available. Please contact Sadie Lawson Photography for a quote.
-    <br>
-    Package Includes:
-    <ul>
-      <li>3 Professionally Edited Photos</li>
-      <li>1 Outfit</li>
-      <li>Professional Backgrounds in Studio</li>
-      <li>Consulting session before the shoot to help you look your best</li>  
-      <li>Digital Gallery</li>
-      <li>Print Rights</li>
-    </ul>
-    <span class="primary-accent-text">Optional Add-Ons:</span>
-    <ul>
-      <li>Additional Photos - $10 Each</li>
-      <li>Additional Outfits - $20 per Outfit</li>
-      <li>On-Location Shoot - $30 (Includes 20 Miles of Travel*)</li>
-      <li>Extended Travel Distance - $10 per 10 Miles</li>
-    </ul>
-    <span class="text-small primary-accent-text">* Mileage is calculated as one-way travel</span>`
+    Group pricing options available. Please contact Sadie Lawson Photography for a quote.`,
+    included: [
+      "3 Professionally Edited Photos",
+      "1 Outfit",
+      "Professional Backgrounds in-studio",
+      "Consulting session before the shoot to help you look your best",
+      "Digital Gallery",
+      "Print Rights"
+    ],
+    addOns: [
+      "Additional Photos - $10 Each",
+      "Additional Outfit - $20",
+      "On-Location Shoot - $30 (Includes 20 Miles of Photographer Travel)*",
+      "Extended Travel Distance - $10 per 10 Miles*"
+    ],
+    finePrint: _mileageStatement
   },
   {
     id: 7,
     name: 'Cherish the Gift of Life Package',
-    subtitle: '',
-    price: '&mdash;Starting at $400&mdash;',
+    price: _formatPrice('Starting at $400'),
     notification: '$100 in Savings',
     description: `Cherish the gift of your child with this amazing offer 
     from SadieLawsonPhotography. This package includes <strong>EVERYTHING</strong> in our Maternity,
     Newborn and Life's Milestone Packages, and a $100 discount. This is the perfect way
-    to celebrate the wonders of your growing family. 
-    <span class="text-small primary-accent-text">*All additional package fees and charges still apply<span>`
+    to celebrate the wonders of your growing family.`,
+    finePrint: '* Additional charges for package add Ons still apply'
   },
   {
     id: 8,
     name: 'Maternity',
     subtitle: '45 Minute Sessions',
-    price: '&mdash;Starting at $150&mdash;',
-    description: `Maternity Sessions Include: 
-    <ul>
-        <li>10+ Curated and Edited Photos*</li>
-        <li>2 Outfits</li>
-        <li>1 Location</li>
-        <li>Digital Gallery</li>
-        <li>Print Rights</li>
-    </ul><span class="primary-accent-text">Optional Add-Ons:</span>
-    <ul>
-      <li>Additional Outfit - $20 per Outfit</li>
-      <li>Additional Location - $20 per Location</li>
-    </ul>`
+    price: _formatPrice('Starting at $150'),
+    included: [
+      "10+ Curated and Edited Photos",
+      "2 Outfits",
+      "1 Location",
+      "20 Miles of Photographer Travel*",
+      "Digital Gallery",
+      "Print Rights"
+    ],
+    addOns: [
+      "Additional Outfit - $20",
+      "Additional Location - $20"
+    ],
+    finePrint: _mileageStatement
   },
   { id: 9,
     name: 'Newborn',
     subtitle: `2-3 Hour Session*`,
-    price: '&mdash;Starting at $200&mdash;',
-    description: `Newborn Sessions Include:
-    <ul>
-        <li>5+ Curated and Edited Photos</li>
-        <li>Unlimited Outfits / Swaddles</li>
-        <li>Studio Location</li>
-        <li>Digital Gallery</li>
-        <li>Print Rights</li>
-    </ul>
-    </ul>
-    <span class="primary-accent-text">Optional Add-Ons:</span>
-    <ul>
-      <li>At-Home Location - $50 (Includes 20 Miles of Photographer Travel*)</li>
-      <li>Extended Travel Distance - $10 per 10 Miles</li>
-      <li>Additional Time - $50 per Hour</li>
-    </ul>
-    <span class="text-small primary-accent-text">* Mileage is calculated as one-way travel</span>`,
+    price: _formatPrice('Starting at $200'),
+    included: [
+      "5+ Curated and Edited Photos",
+      "Unlimited Outfits / Swaddles",
+      "Studio Location",
+      "Digital Gallery",
+      "Print Rights"
+    ],
+    addOns: [
+      "At-Home Location - $50 (Includes 20 Miles of Photographer Travel)*",
+      "Extended Travel Distance - $10 per 10 Miles*",
+      "Additional Time - $50 per Hour"
+    ],
+    finePrint: _mileageStatement
   },
   {
     id: 10,
-    name: 'Life\'s Milestones',
+    name: "Life's Milestones",
     subtitle: '1 hour Sessions',
-    price: '&mdash;Starting at $150&mdash;',
-    description: `It is amazing how fast children grow! Capture the precious moments of your child's
-    first years of life with this amazing deal. Great times to request these sessions are at 6 months, 1 year and 2 years.
-    <br><br>
-    Life\'s Milestones Session Includes: 
-    <ul>
-        <li>10+ Curated and Edited Photos</li>
-        <li>1 Outfit</li>
-        <li>1 Location</li>
-        <li>Digital Gallery</li>
-        <li>Print Rights</li>
-    </ul>    
-    <span class="primary-accent-text">Optional Add-Ons:</span>
-    <ul>
-      <li>Cake Smash - $50</li>
-      <li>Additional Outfit - $20 per Outfit</li>
-      <li>At-Home Location - $50 (Includes 20 Miles of Photographer Travel*)</li>
-      <li>Extended Travel Distance - $10 per 10 Miles</li>
-      <li>Additional Time - $50 per Hour</li>
-    </ul>
-    <span class="text-small primary-accent-text">* Mileage is calculated as one-way travel</span>`
+    price: _formatPrice('Starting at $150'),
+    description: `It is amazing how fast children grow! Capture the precious moments of your child's first years of life 
+    with this amazing deal. Great times to request these sessions are at 6 months, 1 year and 2 years.`,
+    included: [
+      "10+ Curated and Edited Photos",
+      "1 Outfit",
+      "1 Location",
+      "Digital Gallery",
+      "Print Rights"
+    ],
+    addOns: [
+      "Cake Smash - $50",
+      "Additional Outfit - $20",
+      "At-Home Location - $50 (Includes 20 Miles of Photographer Travel)*",
+      "Extended Travel Distance - $10 per 10 Miles*",
+      "Additional Time - $50 Per Hour"
+    ],
+    finePrint: _mileageStatement
   }
 ];
